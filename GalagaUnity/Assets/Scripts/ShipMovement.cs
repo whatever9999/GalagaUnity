1acdc60fea66cb9208f4e08c97b58878d543ae4f
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2019-01-21 16:26:21 +0000

    Added base files from first class

---
+++ GalagaUnity/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float Speed = 1;
+    public float RotateSpeed = 5;
+    
+    void Update()
+    {
+        //Movement      
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-(Speed * Time.deltaTime), 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate((Speed * Time.deltaTime), 0, 0);
+        }
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(0, (Speed * Time.deltaTime), 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, -(Speed * Time.deltaTime), 0);
+        }
+
+        //Rotation
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime);
+    }
+}
